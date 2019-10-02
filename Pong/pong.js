b4da89178c7246e7b731405bc337ab72053ca942
nicku3d <nicku3d@gmail.com>
2019-10-02 14:51:38 +0200

    Ball movement now is based on trigonometry

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-
 let c;
 let ctx;
 let interval = undefined;
@@ -11,22 +10,27 @@ const gridSize = 20;
 
 
 const player = {
-    x : 0,
-    y : 0,
-    direction : 0,
-    w : gridSize,
-    h : gridSize*4,
-    speed : 7,
+    x: 0,
+    y: 0,
+    direction: 0,
+    w: gridSize,
+    h: gridSize * 4,
+    speed: 7,
 }
+let angle = Math.round(Math.random()*360);
+let bounceAngle = angle * Math.PI / 180;
+console.log(angle);
 
 const ball = {
-    x : 0,
-    y : 0,
-    xv : -1, //x velocity
-    yv : 0, //y velocity
-    speed : 7,
-    size : gridSize,
+    x: 0,
+    y: 0,
+    xv: Math.cos(bounceAngle), //x velocity
+    yv: -Math.sin(bounceAngle), //y velocity
+    angle: 0,
+    speed: 7,
+    size: gridSize,
 }
+
 
 
 
@@ -38,37 +42,35 @@ window.onload = () => {
     ctx.fillStyle = "white";
     //init player position
     player.x = 50;
-    player.y = c.height/2-(2*gridSize);
+    player.y = c.height / 2 - (2 * gridSize);
 
-    ball.x = c.width/2;
-    ball.y = c.height/2;
+    ball.x = c.width / 2;
+    ball.y = c.height / 2;
     //end of init player position
 
     //init ball position
-    
+
 
     //end of init ball position
     showText("Press Enter to start!");
 
-    document.addEventListener("keydown",keyDownHandler);
+    document.addEventListener("keydown", keyDownHandler);
     document.addEventListener("keyup", keyUpHandler);
 
 }
 
-function gameLoop(){
+function gameLoop() {
     //playerMovement
-    if(player.direction == -1) {
+    if (player.direction == -1) {
         player.y += player.speed;
-    }
-    else if (player.direction == 1) {
+    } else if (player.direction == 1) {
         player.y -= player.speed;
     }
 
     //player collison check
-    if(player.y > c.height-player.h){
-        player.y = c.height-player.h;
-    }
-    else if ( player.y < 0 ){
+    if (player.y > c.height - player.h) {
+        player.y = c.height - player.h;
+    } else if (player.y < 0) {
         player.y = 0;
     }
     //end of player collision check
@@ -91,13 +93,13 @@ function gameLoop(){
     //end of ball collison check and movement
 
     //player - ball collision
-    if(ball.x <= (player.x+player.w)
-        && ball.y > player.y-ball.size
-        && ball.y < player.y+player.h){
-        ball.xv = 1;
-    }
-
+    // if (ball.x <= (player.x + player.w)
+    //     && ball.y > player.y - ball.size
+    //     && ball.y < player.y + player.h) {
+    //     ball.xv = 1;
+    // }
     //end of player - ball collision
+
     //debug
     //console.log("X " + player.x +" ==? "+ ball.x);
     //console.log("Y" + player.y +" "+ (player.y+player.h) +" ==? "+ ball.y);
@@ -110,46 +112,51 @@ function gameLoop(){
 
 }
 
-function moveBall(){
-    // if(ball.x <= c.width){
-    //     ball.x += 5;
-    // }
-    //
-    // if(ball.y <= c.height){
-    //     ball.y += 5;
-    // }
+function moveBall() {
+
+    //ballVx = BALLSPEED*Math.cos(bounceAngle);
+    // ballVy = BALLSPEED*-Math.sin(bounceAngle);
+
+
     if(ball.x >= c.width - gridSize*1.2){
-        ball.xv = -1;
-    }else if ( ball.x <= 0){
-        ball.xv = 1;
+        ball.xv=-ball.xv;
     }
+    else if( ball.x <= 0){
+        ball.xv=-ball.xv;
+    }
+
     if(ball.y >= c.height - gridSize*1.2){
-        ball.yv = -1;
+        ball.yv = -ball.yv;
     }
     else if(ball.y <= 0){
-        ball.yv = 1;
+        ball.yv = -ball.yv;
     }
 
+    ball.x+=ball.speed*ball.xv;
+    ball.y+=ball.speed*ball.yv;
 
-    //adjust speed
-    if(ball.xv == 1) {
-        ball.x += ball.speed;
-    }
-    if(ball.xv == -1){
-        ball.x -= ball.speed;
-    }
 
-    if(ball.yv == 1){
-        ball.y += ball.speed;
-    }
-    if(ball.yv == -1){
-        ball.y -= ball.speed;
-    }
+    //
+    //
+    // //adjust speed
+    // if(ball.xv == 1) {
+    //     ball.x += ball.speed;
+    // }
+    // if(ball.xv == -1){
+    //     ball.x -= ball.speed;
+    // }
+    //
+    // if(ball.yv == 1){
+    //     ball.y += ball.speed;
+    // }
+    // if(ball.yv == -1){
+    //     ball.y -= ball.speed;
+    // }
     //end of adjusting speed
 }
 
-function keyDownHandler(event){
-    switch(event.key){
+function keyDownHandler(event) {
+    switch (event.key) {
         case "ArrowUp":
             player.direction = 1;
             break;
@@ -157,15 +164,15 @@ function keyDownHandler(event){
             player.direction = -1;
             break;
         case "Enter":
-            if(interval == undefined){
+            if (interval == undefined) {
                 interval = setInterval(gameLoop, gameTimeout);
             }
             break;
     }
 }
 
-function keyUpHandler(event){
-    switch(event.key){
+function keyUpHandler(event) {
+    switch (event.key) {
         case "ArrowUp":
             player.direction = 0;
             break;
@@ -176,19 +183,19 @@ function keyUpHandler(event){
     }
 }
 
-function drawBall(){
+function drawBall() {
     ctx.fillStyle = "white";
     ctx.fillRect(ball.x, ball.y, gridSize, gridSize);
 }
 
-function drawPlayer(){
+function drawPlayer() {
     ctx.fillStyle = "white";
-    ctx.fillRect(player.x, player.y, gridSize, 4*gridSize );
+    ctx.fillRect(player.x, player.y, gridSize, 4 * gridSize);
 }
 
-function doCoolStuff(){
-    for(let x = 0;x < c.width; x+=gridSize){
-        for(let y = 0; y < c.height ; y+=gridSize){
+function doCoolStuff() {
+    for (let x = 0; x < c.width; x += gridSize) {
+        for (let y = 0; y < c.height; y += gridSize) {
             ctx.fillStyle = () => {
                 let hexChars = '0123456789ABCDEF';
                 let color = '#';
@@ -197,30 +204,29 @@ function doCoolStuff(){
                 }
                 return color;
             }
-            ctx.fillRect(x, y,gridSize,gridSize);
+            ctx.fillRect(x, y, gridSize, gridSize);
         }
     }
 }
 
-function drawGrid(){
+function drawGrid() {
     ctx.strokeStyle = "red";
-    for(let x = 0;x < c.width; x+=gridSize){
-        for(let y = 0; y < c.height ; y+=gridSize) {
+    for (let x = 0; x < c.width; x += gridSize) {
+        for (let y = 0; y < c.height; y += gridSize) {
             ctx.strokeRect(x, y, gridSize, gridSize);
         }
     }
 }
 
 
-
-function drawBackground(){
+function drawBackground() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.fillStyle = "white";
-    ctx.fillRect(c.width/2, 0, 10, c.height);
+    ctx.fillRect(c.width / 2, 0, 10, c.height);
 }
 
-function showText(text, x = 180 , y = 200, color="green"){
+function showText(text, x = 180, y = 200, color = "green") {
     ctx.fillStyle = color;
     ctx.font = "30px Arial";
     ctx.fillText(text, x, y);
